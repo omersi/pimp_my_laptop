225d131c64de3438ed53dba2f352b976670de525
Omer Livne <omer@livne.me>
2022-06-21 10:49:05 +0300

    Create install_spotify.sh
    Install spotify player and kdocker to minimise the player to ubuntu tray

---
+++ install_spotify.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Install KDocker
+sudo apt install kdocker
+
+# Install Spotify
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+
+sudo apt-get update && sudo apt-get install spotify-client
+
+# Create minimized spotify
+SPOTIFY_ICON=$(find /usr/share -name "spotify-client.png" | grep 64)
+
+# Create spotify.desktop
+cat <<EOF > $HOME/.local/share/applications/spotify.desktop
+[Desktop Entry]
+Type=Application
+Name=Spotify
+GenericName=Music Player
+Icon=spotify-client
+TryExec=spotify
+Exec=kdocker -q -o -l -i ${SPOTIFY_ICON} -n Spotify spotify %U
+Terminal=false
+MimeType=x-scheme-handler/spotify;
+Categories=Audio;Music;Player;AudioVideo;
+StartupWMClass=spotify
+EOF
